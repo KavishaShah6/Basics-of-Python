34bea38c33154c333280c535a4824e70115bb3a4
Kavisha Shah <kavisha653@gmail.com>
2025-07-30 11:06:38 +0530

    Variables and Datatypes

---
+++ VarialesAndDatatypes/variables.py
@@ -0,0 +1,14 @@
+name = "XYZ"
+age = 22
+height = 5.4
+is_student = True
+
+print("Name:", name)
+print("Age:", age)
+print("Height:", height)
+print("Is student:", is_student)
+print()
+print(type(name))
+print(type(age))
+print(type(height))
+print(type(is_student))
